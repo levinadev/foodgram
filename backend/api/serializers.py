5dc3dc181f3d7e0a9367bd3465d5694e3272bcb5
levinadevx <anna_levina@internet.ru>
2025-08-20 14:35:28 +0300

    feat: добавлена авториация через djoser

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,7 +5,7 @@ from users.models import User
 class UserSerializer(serializers.ModelSerializer):
     """Сериализатор для всех операций с пользователями."""
 
-    password = serializers.CharField(write_only=True, required=False)
+    password = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
