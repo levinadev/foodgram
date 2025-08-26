0e6d8b9640ef5deaeeb55bb587dec06ba87f1b64
levinadevx <anna_levina@internet.ru>
2025-08-26 11:16:24 +0300

    feat: добавлен метод на удаление аватара

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,7 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
 from .serializers import AvatarSerializer
+
 
 class AvatarUpdateView(generics.UpdateAPIView):
     serializer_class = AvatarSerializer
@@ -7,3 +9,8 @@ class AvatarUpdateView(generics.UpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+    def delete(self, request, *args, **kwargs):
+        user = self.get_object()
+        user.avatar.delete(save=True)
+        return Response(status=status.HTTP_204_NO_CONTENT)
