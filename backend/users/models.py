35a120add9424a6145094a2965b4542df02e887b
levinadev <anna_levina@internet.ru>
2025-08-27 03:10:51 +0300

    feat: добавлена модель Subscription

--- backend/users/models.py
+++ backend/users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    """Кастомная модель пользователя с логином по email."""
+    """Кастомная модель пользователя с логином по email"""
     username = models.CharField(
         max_length=150,
         unique=True,
@@ -23,3 +23,21 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
+
+
+class Subscription(models.Model):
+    """Модель подписки пользователя на автора"""
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="subscriptions"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="subscribers"
+    )
+
+    objects = models.Manager()
+
+    class Meta:
+        unique_together = ("user", "author")
+
+    def __str__(self):
+        return f"{self.user} → {self.author}"
