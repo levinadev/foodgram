cdedc401016b73941123ab28ea0b1f5dc24cc91a
levinadev <anna_levina@internet.ru>
2025-08-27 17:19:30 +0300

    feat: добавлена модель Tag

--- backend/tags/models.py
+++ backend/tags/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    """Модель тегов (меток) для поиска данных"""
+    name = models.CharField(
+        max_length=32,
+        unique=True,
+    )
+    slug = models.SlugField(
+        max_length=32,
+        unique=True,
+    )
+
+    objects = models.Manager()
+
+    class Meta:
+        ordering = ["id"]
+
+    def __str__(self):
+        return self.name
