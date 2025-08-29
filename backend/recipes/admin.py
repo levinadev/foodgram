3366154d6fa9c94fc54a14e6c8c92d50f4c246f7
levinadev <anna_levina@internet.ru>
2025-08-29 15:46:21 +0300

    feat: перенесена ingredients, удалены старые миграции

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Ingredient, Recipe, RecipeIngredient
+from .models import Recipe, RecipeIngredient
 
-admin.site.register(Ingredient)
 admin.site.register(Recipe)
 admin.site.register(RecipeIngredient)
