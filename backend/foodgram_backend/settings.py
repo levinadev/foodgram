82a1c0b4d0f8bc540c02435f9c2dc13c128aee60
levinadev <anna_levina@internet.ru>
2025-08-29 17:39:04 +0300

    feat: добавлен фильтр для поиска ингредиента по подстроке

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -104,7 +104,8 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_FILTER_BACKENDS": [
-        "django_filters.rest_framework.DjangoFilterBackend"
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 6,
