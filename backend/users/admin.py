81bbffc631a1005af63205851f9fa1382dbc78d0
levinadev <anna_levina@internet.ru>
2025-08-30 05:47:27 +0300

    fix: добавлен метод для хэширования пароля в админке

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.hashers import make_password
 from .models import User, Subscription
 
-admin.site.register(User)
+@admin.register(User)
+class CustomUserAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        if not change or "password" in form.changed_data:
+            obj.password = make_password(obj.password)
+        super().save_model(request, obj, form, change)
+
 admin.site.register(Subscription)
