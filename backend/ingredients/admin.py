fc45f63a2a5b92504d2d981201cfdf7b767da6ad
levinadev <anna_levina@internet.ru>
2025-08-30 21:16:46 +0300

    fix: настроено админка

--- backend/ingredients/admin.py
+++ backend/ingredients/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Ingredient
 
-admin.site.register(Ingredient)
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ("name", "measurement_unit")
+
+    search_fields = ("name",)
+
+
+admin.site.register(Ingredient, IngredientAdmin)
--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
+from django.db.models import Count
+from .models import Recipe, RecipeIngredient, Favorite, ShoppingCart
+from tags.models import Tag
 
-from .models import (
-    Recipe,
-    RecipeIngredient,
-    Favorite,
-    ShoppingCart
-)
 
-admin.site.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ("name", "author", "get_favorites_count")
+    search_fields = ("name", "author__username", "author__email")
+    list_filter = ("tags",)
+
+    def get_favorites_count(self, obj):
+        return obj.favorites.count()
+
+    get_favorites_count.short_description = "Favorite Count"
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.annotate(favorite_count=Count("favorites"))
+
+
+admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(RecipeIngredient)
 admin.site.register(Favorite)
 admin.site.register(ShoppingCart)
