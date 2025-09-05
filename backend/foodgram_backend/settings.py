c55305a73eca54f60330f9b20d79ade15f4f38ab
levinadevx <anna_levina@internet.ru>
2025-09-05 18:25:48 +0300

    fix: удалена sqlite из настроек

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -62,13 +62,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram_backend.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
